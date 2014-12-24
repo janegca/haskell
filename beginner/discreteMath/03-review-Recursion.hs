b418ceef792d6e0a62cccfaa1b607e43fd6476cb
janegca <jgriscti@gmail.com>
2014-12-24 10:44:27 -0500

    initial commit

--- beginner/discreteMath/03-review-Recursion.hs
+++ beginner/discreteMath/03-review-Recursion.hs
@@ -3,7 +3,8 @@
 -- Reference: 'Discrete Mathematics Using a Computer' by John O'Donnell,
 --             Cordelia Hall, and Rex Page
 
-{- 15. Write a function that takes two lists, and returns a list of values
+{-  
+   15. Write a function that takes two lists, and returns a list of values
        that appear in both lists. The function should have type 
        
             intersection :: Eq a => [a] -> [a] -> [a].
@@ -22,7 +23,7 @@ intersection (x:xs) (y:ys)
     | elem y xs                  = y : intersection xs ys
     | otherwise                  = intersection xs ys
     
-{-
+{- ------------------------------------------------------------------------
     16. Write a function that takes two lists, and returns True if all the
         elements of the first list also occur in the other. The function 
         should have type:
@@ -42,7 +43,7 @@ isSubset xs []     = False
 isSubset [] ys     = True
 isSubset (x:xs) ys = elem x ys && isSubset xs ys
 
-{-
+{- -----------------------------------------------------------------------
     17. Write a recursive function that determines whether a list is
         sorted.
 -}
@@ -51,3 +52,56 @@ isSorted []       = True
 isSorted (x:[])   = True 
 isSorted (x:y:zs) = x < y && isSorted (y:zs)
 
+{- -----------------------------------------------------------------------
+    18. Show that the definition of factorial using foldr always produces
+        the same result as the recursive definition given in the previous
+        section.
+        
+        Base Case:
+            foldr (*) 1 [] = 1      -- def of foldr, returns zero value
+                                    -- for empty list argument
+            factorial 0 = 1         -- def of factorial
+            
+        Recursive Case:
+            Note that the definition of factorial
+                factorial n+1 = n+1 * factorial n
+            ==> factorial n   = n   * factorial n-1
+            
+            Let k = n - 1,
+        
+              foldr (*) 1 [1..n]    -- arg is list excluding the base case
+            = foldr (*) 1 (1 :(2 :(3 : ... :( n : []))))  -- def of list
+            = (1 * (2 * (3 * ... *( n * 1 ))))            -- apply foldr
+            = (1 * (2 * (3 * ... * n)))       -- arithmetic
+            = n * (1 * (2 * (3 * ... * k)))   -- assoc. property of mult    
+            = n * factorial n-1               -- def of factorial           
+-}
+{- -----------------------------------------------------------------------
+   19. Using recursion, define last, a function that takes a list and
+       returns a Maybe type that is Nothing if the list is empty
+    
+-}
+last' :: [a] -> Maybe a
+last' []     = Nothing
+last' (x:[]) = Just x
+last' (x:xs) = last' xs
+
+{- -----------------------------------------------------------------------
+    20. Using recursion, write two functions that expect a string containing
+        a number that contains a decimal point (for example, 23.455).
+        
+        The first function returns the whole part of the number (i.e., the
+        part to the left of the decimal point). The second function 
+        returns the fractional part (the part to the right of the decimal 
+        point).
+-}
+wholeNum :: String -> String
+wholeNum []     = ""
+wholeNum (x:xs) | x == '.' = []
+                | otherwise = x : wholeNum xs
+               
+fracNum :: String -> String
+fracNum [] = ""
+fracNum (x:xs) | x == '.'  = xs
+               | otherwise = fracNum xs
+               
---
+++ beginner/discreteMath/m-quadratic.hs
@@ -0,0 +1,30 @@
+-- quadratic function
+-- solves the expression: x * x^2 + b * x + c = 0 with two real solutions
+-- (x1,x2)
+
+quadratic :: Double -> Double -> Double -> (Double,Double)
+quadratic a b c
+    = let d = sqrt (b^2 - 4*a*c)
+          x1 = (-b + d) / (2*a)
+          x2 = (-b - d) / (2*a)
+      in (x1,x2)
+      
+{-
+    Example Usage
+    
+        *Main> quadratic' 2 5 (-3)
+        Just (0.5,-3.0)
+        *Main> quadratic' 2 4 (-4)
+        Just (0.7320508075688772,-2.732050807568877)
+        *Main> quadratic' 0 4 (-4)
+        Nothing    
+-}      
+      
+quadratic' :: Double -> Double -> Double -> Maybe (Double,Double)
+quadratic' a b c =
+    if a == 0 then Nothing
+    else let d = sqrt (b^2 - 4*a*c)
+             x1 = (-b + d) / (2*a)
+             x2 = (-b - d) / (2*a)
+         in (Just (x1,x2))
+      
